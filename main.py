3aca6269de9740418251314b111645f8e3654048
Prakash Kumar <prakash218kumar@gmail.com>
2022-07-07 14:00:09 +0530

    Final push

--- main.py
+++ main.py
@@ -1,23 +1,282 @@
-from flask import Flask, render_template, request, redirect, url_for, flash, jsonify
+from dateutil import parser
+from flask import Flask, render_template, session, request, redirect, url_for, flash, jsonify
 from flask_restful import Api, Resource, abort, fields, marshal_with
 from flask_sqlalchemy import SQLAlchemy, Model
 
 from werkzeug.security import generate_password_hash, check_password_hash
 
-app = Flask(__name__)
-app.secret_key = "Prakash Presidio Project"
-app.config['TEMPLATES_AUTO_RELOAD'] = True
 
-api = Api(app)
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///db.sqlite'
-db = SQLAlchemy(app)
+from flask_login import UserMixin, LoginManager, login_user, current_user, logout_user
+from datetime import datetime
+
+from flask_wtf import FlaskForm
+from wtforms import StringField,PasswordField,SubmitField,BooleanField
+from wtforms.validators import DataRequired,Email,EqualTo
+
+app                                     = Flask(__name__)
+app.secret_key                          = "Prakash Presidio Project"
+app.config['TEMPLATES_AUTO_RELOAD']     = True
+
+api                                     = Api(app)
+app.config['SQLALCHEMY_DATABASE_URI']   = 'sqlite:///db.sqlite'
+db                                      = SQLAlchemy(app)
+login_manager                           = LoginManager()
+login_manager.init_app(app)
+
+class User(UserMixin, db.Model):
+  id            = db.Column(db.Integer, primary_key=True)
+  name          = db.Column(db.String(50))
+  email         = db.Column(db.String(150), unique = True, index = True)
+  password_hash = db.Column(db.String(150))
+  joined_at     = db.Column(db.DateTime(), default = datetime.utcnow, index = True)
+
+  def set_password(self, password):
+        self.password_hash = generate_password_hash(password)
+
+  def check_password(self,password):
+      return check_password_hash(self.password_hash,password)
+  @classmethod
+  def get(self, id):
+    return self.query.get(id)
+
+class Flight(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(120))
+    from_ = db.Column(db.String(120))
+    to_   = db.Column(db.String(120))
+    date = db.Column(db.DateTime())
+    time = db.Column(db.String(120))
+    tickets = db.Column(db.Integer)
+    price = db.Column(db.Integer)
+    
+    def __repr__(self) :
+        return f'Flight-{self.id} {self.from_}->{self.to_} on {self.date} at {self.time}'
+    
+    def _asdict(self):
+        return {
+            'id': self.id,
+            'name': self.name,
+            'from_': self.from_,
+            'to_': self.to_,
+            'date': str(self.date),
+            'time': self.time,
+            'price': self.price,
+            'tickets': self.tickets,
+            'date_' : self.date.strftime('%d-%b-%Y')
+        }
+
+class Ticket(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    flight_id = db.Column(db.Integer, db.ForeignKey('flight.id'))
+    tickets = db.Column(db.Integer)
+    price = db.Column(db.Integer) 
+    user = db.Column(db.Integer, db.ForeignKey('user.id')) 
+    
+    def _asdict(self):
+        return {
+            'id': self.id,
+            'flight_id': self.flight_id,
+            'tickets': self.tickets,
+            'price' : self.price,
+            'user' : self.user
+        }
+# db.create_all()
+    
+
+class RegistrationForm(FlaskForm):
+    name = StringField('username',                  validators =[DataRequired()])
+    email = StringField('Email',                    validators=[DataRequired(),Email()])
+    password1 = PasswordField('Password',           validators = [DataRequired()])
+    password2 = PasswordField('Confirm Password',   validators = [DataRequired(),EqualTo('password1')])
+    submit = SubmitField('Register')
+
+class LoginForm(FlaskForm):
+    email = StringField('Email',                    validators=[DataRequired(), Email()])
+    password = PasswordField('Password',            validators=[DataRequired()])
+    remember = BooleanField('Remember Me',          default = False)
+    submit = SubmitField('Login')
+
+class BOOK_FLIGHT(Resource):
+    
+    def get(self):
+        id = current_user.id
+        tickets = Ticket.query.filter_by(user = id).all()
+        
+        for idx in range(len(tickets)):
+            tickets[idx] = tickets[idx]._asdict()
+            flight_details = Flight.query.filter_by(id = tickets[idx]['flight_id']).first()
+            for detail in flight_details._asdict().keys():
+                if detail == 'tickets':continue
+                if detail == 'date':continue
+                if detail == 'price':continue
+                #     tickets[idx][detail] = str(flight_details._asdict()[detail].date()).split(' ')[0]
+                # else:
+                tickets[idx][detail] = flight_details._asdict()[detail]
+        print(tickets)
+        return jsonify(tickets)
+        
+    
+    def put(self):
+        args = request.get_json()
+        ticket = Ticket(flight_id = args['id'], tickets = args['ticket'], price = args['price'], user = current_user.id)
+        flight = Flight.query.get(args['id'])
+        flight.tickets -= args['ticket']
+        db.session.add(ticket)
+        db.session.commit()
+        
+        return jsonify({'message': 'Ticket Booked'})
+    
+class REMOVE_FLIGHT_API(Resource):
+    def post(self):
+        data = request.get_json()
+        flight = Flight.query.get(data['id'])
+        print('\n' * 10)
+        print(flight)
+        print('\n' * 10)
+        db.session.delete(flight)
+        db.session.commit()
+        return {'message': 'Flight added successfully'}, 201   
+    
+class SEARCH_FLIGHT(Resource):
+    def get(self):
+        args = request.args
+        if args['date'] != '':
+            date = parser.parse(args['date'])
+        else:
+            date = ''
+        
+        flights = Flight.query.all()
+        print(flights)
+        if args['from'] != ' ':
+            flights = list(map(lambda x: x, filter(lambda x: x.from_.lower() == args['from'].lower(), flights)))
+        if args['to'] != ' ':
+            flights = list(map(lambda x: x, filter(lambda x: x.to_.lower() == args['to'].lower(), flights)))
+        if date != '':
+            flights = list(map(lambda x: x, filter(lambda x: x.date == date, flights)))
+        print(flights)
+        for idx in range(len(flights)):
+            flights[idx] = flights[idx]._asdict()
+        return jsonify(flights)
+        
+class CREATE_FLIGHT(Resource):
+    
+    def put(self):
+        args = request.get_json()
+        date = parser.parse(args['date']).date()
+        print(args)
+        flight = Flight(name = args['name'], from_ = args['from'], to_ = args['to'], date = date, time = args['time'], price = args['price'], tickets = 60)
+        print(flight)
+        db.session.add(flight)
+        db.session.commit()
+        print('commited successfully')
+        return {"status": "success", "object": flight._asdict()}
+
+class VIEW_FLIGHT(Resource):
+    
+    def get(self):
+        args = request.args
+        
+        print('here', args)
+        tickets = Ticket.query.filter_by(flight_id = args['id']).all()
+        for idx in range(len(tickets)):
+            tickets[idx] = tickets[idx]._asdict()
+            flight_details = Flight.query.filter_by(id = tickets[idx]['flight_id']).first()
+            for detail in flight_details._asdict().keys():
+                if detail == 'tickets':continue
+                if detail == 'date':continue
+                if detail == 'price':continue
+                #     tickets[idx][detail] = str(flight_details._asdict()[detail].date()).split(' ')[0]
+                # else:
+                tickets[idx][detail] = flight_details._asdict()[detail]
+            tickets[idx]['booking_name'] = User.query.filter_by(id = tickets[idx]['user']).first().name
+        print(tickets)
+        return jsonify(tickets)
+
+def auth():
+    return current_user.is_authenticated
+    
+@login_manager.user_loader
+def load_user(user_id):
+    return User.get(user_id)
 
 
+
+@app.route('/account')
+def account():
+    return render_template('account.html')
 
 @app.route('/')
 @app.route('/index')
 def index():
-    return render_template('index.html')
+    if auth():
+        return render_template('index.html', title = "Home", user = current_user)
+    return redirect(url_for('login'))
+
+@app.route('/register', methods = ['POST','GET'])
+def register():
+    form = RegistrationForm()
+    if form.validate_on_submit():
+        user = User(name =form.name.data, email = form.email.data)
+        user.set_password(form.password1.data)
+        db.session.add(user)
+        db.session.commit()
+        return redirect(url_for('login'))
+    return render_template('registration.html', form=form)
+
+@app.route('/admin_login', methods = ['POST','GET'])
+def admin_login():
+    form = LoginForm()
+    if form.validate_on_submit():
+        user = User.query.filter_by(email = form.email.data).first()
+        if user and user.check_password(form.password.data) and user.email == "admin@admin.com":
+            login_user(user, remember = form.remember.data)
+            return redirect(url_for('index'))
+        else:
+            flash('Invalid Credentials')
+    return render_template('admin-login.html', form=form)
+
+@app.route('/login', methods=['GET', 'POST'])
+def login():
+
+    form = LoginForm()
+    if form.validate_on_submit():
+        user = User.query.filter_by(email = form.email.data).first()
+        if user is not None and user.check_password(form.password.data):
+            login_user(user)
+            next = request.args.get("next")
+            return redirect(next or url_for('index'))
+        flash('Invalid email address or Password.')    
+    return render_template('login.html', form=form)
+
+@app.route('/add_flight')
+def add_flight():
+    if current_user.email == "admin@admin.com":
+        return render_template('add_flight.html')
+    return redirect(url_for('index'))
+
+@app.route('/logout')
+def logout():
+    logout_user()
+    return redirect(url_for('index'))
+
+
+@app.route('/remove_flight')
+def remove_flight():
+    if current_user.email == "admin@admin.com":
+        return render_template('remove_flight.html')
+    return redirect(url_for('index'))
+
+@app.route('/view')
+def view():
+    if current_user.email == "admin@admin.com":
+        return render_template('view.html')
+    return redirect(url_for('index'))
+
+api.add_resource(CREATE_FLIGHT, '/create_flight')
+api.add_resource(SEARCH_FLIGHT, '/search')
+api.add_resource(REMOVE_FLIGHT_API, '/remove')
+api.add_resource(BOOK_FLIGHT, '/book')
+api.add_resource(VIEW_FLIGHT, '/flights')
 
 if __name__ == '__main__':
     app.run(debug = True, port = 5000)
---
+++ static/js/script.js
@@ -0,0 +1,371 @@
+
+
+async function view(id)
+{
+    var url = `http://localhost:5000/flights?id=${id}`;
+    try
+    {
+        var response = await fetch(url);
+        var data = await response.json();
+        
+
+
+
+        if(data)
+        {
+            var headDIV = document.getElementById("detailheader");
+
+            var div = document.getElementById("results");
+            div.innerHTML = "";
+            headDIV.innerHTML = `
+            <h2>${data[0].from_} → ${data[0].to_}</h2>
+            <p><strong>Date : </strong>${data[0].date_} <strong>Time : </strong> ${data[0].time} <strong> Flight Name: </strong> ${data[0].name}</p>
+            `
+
+            headDIV.style.display = "block";
+
+            for(var i = 0; i < data.length; i++)
+            {
+
+                var template = `<div class="card mb-3" style="max-width: 540px;">
+                <div class="row no-gutters">
+                  <div class="col-md-4">
+                    <img src="static/img/ticket.jpg" class="card-img" alt="...">
+                  </div>
+                  <div class="col-md-8">
+                    <div class="card-body">
+                      <h5 class="card-title">User : ${data[i].booking_name}</h5>
+                      <p class="card-text">
+                      <strong>Tickets: </strong>${data[i].tickets} <br>
+                      <strong>Price: </strong>Rs.${data[i].price} <br>
+                      </p>
+                    </div>
+                  </div>
+                </div>
+              </div>`
+                div.innerHTML += template;
+            }
+        }
+    }
+    catch(error)
+    {
+        console.log(error);
+    }
+}
+
+function create_my_account_divs(data)
+{
+    var div = document.getElementById("account_container");
+    div.innerHTML = "";
+    for(var i = 0; i < data.length; i++)
+    {
+        var template = `<div class="card mb-3" style="max-width: 540px;">
+        <div class="row no-gutters">
+          <div class="col-md-4">
+            <img src="static/img/ticket.jpg" class="card-img" alt="...">
+          </div>
+          <div class="col-md-8">
+            <div class="card-body">
+              <h5 class="card-title">${data[i].from_} → ${data[i].to_}</h5>
+              <p class="card-text">
+              <strong>Date:</strong> ${data[i].date_}   
+              <strong>Time:</strong> ${data[i].time}<br>
+              <strong>Flight Name: </strong>${data[i].name}<br>
+              <strong>Tickets: </strong>${data[i].tickets} <br>
+              <strong>Price: </strong>Rs.${data[i].price} <br>
+              </p>
+            </div>
+          </div>
+        </div>
+      </div>`
+        div.innerHTML += template;
+    }
+
+}
+
+async function my_account()
+{
+    var url = `http://localhost:5000/book`;
+
+    try{
+        var response = await fetch(url);
+        var data = await response.json();
+        console.log(data);
+        if(data)
+        {
+            create_my_account_divs(data);
+        }
+        
+    }
+    catch(error) {
+        console.log(error);
+    }
+}
+
+async function book_flight(id, price, tickets)
+{
+    console.log('here');
+    var modal = document.getElementById('myModal');
+    modal.style.display = "block";
+
+    var span = document.getElementsByClassName("close")[0];
+    span.onclick = function() {
+        modal.style.display = "none";
+    }
+
+    var price_span = document.getElementById("price");
+    var ticket_input = document.getElementById("number");
+
+    var book_button = document.getElementById("confirm");
+
+    book_button.onclick = async function() {
+        var ticket = parseInt(ticket_input.value);
+        if(ticket > tickets)
+        {
+            alert("Not enough tickets");
+        }
+        else
+        {
+            var url = `http://localhost:5000/book?id=${id}&ticket=${ticket}`;
+            try
+            {
+                var response = await fetch(url, {
+                    method: 'PUT',
+                    headers: {
+                        'Content-Type': 'application/json'
+                    },
+                    body: JSON.stringify({id: id, ticket: ticket, price: price * ticket})
+                });
+
+                if (response) {
+                    var data = await response.json();
+                    console.log(data);
+                    alert("Flight booked successfully");
+                    modal.style.display = "none";
+                }
+                else {
+                    alert("Error booking flight");
+                    modal.style.display = "none";
+                }
+                location.reload();
+            }
+            catch(error)
+            {
+                console.log(error);
+                modal.style.display = "none";
+            }
+        }
+    }
+
+    // ticket_input.onchange = function()
+    // {
+    //     console.log(ticket_input.value, price);
+    //     var cost = parseInt(price) * parseInt(ticket_input.value);
+    //     price_span.innerHTML = "Price : Rs. " + cost;
+    // }
+
+    ticket_input.addEventListener('input', function() {
+        console.log(ticket_input.value, price);
+        var cost = parseInt(price) * (parseInt(ticket_input.value) || 0);
+        price_span.innerHTML = "Price : Rs. " + cost;
+    });
+
+}
+
+async function remove_flight(id)
+{
+    if(confirm("Are you sure you want to delete this?"))
+    {
+        var url = `http://localhost:5000/remove?id=${id}`;
+        try
+        {
+            var response = await fetch(url, {
+                method: 'POST',
+                headers: {
+                    'Content-Type': 'application/json'
+                },
+                body: JSON.stringify({id: id})
+            });
+
+            if (response) {
+                var data = await response.json();
+                console.log(data);
+                alert("Flight deleted successfully");
+            }
+            else {
+                alert("Error deleting flight");
+            }
+        }
+        catch(error)
+        {
+            console.log(error);
+        }
+    }
+    else
+    {
+        return;
+    }
+}
+
+function create_divs(data, op)
+{
+    var div = document.getElementById("results");
+    div.innerHTML = "";
+    for(var i = 0; i < data.length; i++)
+    {
+        if(op == "search")
+        {
+            const date = data[i]['date'].split(/(\s+)/);
+            var template = `<div class="card" style="width: 18rem;">
+            <img class="card-img-top" src="static/img/flight.jpg" alt="Card image cap">
+            <div class="card-body">
+              <h5 class="card-title">${data[i]['from_']} → ${data[i]['to_']}</h5>
+              <h6 class="card-subtitle mb-2 text-muted">Rs.${data[i].price}</h6>
+              <p class="card-text">Flight Name : ${data[i]['name']}</p>
+            </div>
+            <ul class="list-group list-group-flush">
+              <li class="list-group-item">Date : ${date[0]}</li>
+              <li class="list-group-item">Time : ${data[i]['time']}</li>
+              <li class="list-group-item">Tickets : ${data[i]['tickets']}</li>
+            </ul>
+            
+            <div class="card-body">
+                <button class="btn btn-dark" onclick="book_flight(${data[i].id},${data[i].price}, ${data[i].tickets})">Book</button>
+            </div>
+          </div>`
+        }
+        else if(op == "remove")
+        {
+            const date = data[i]['date'].split(/(\s+)/);
+            var template = `<div class="card" style="width: 18rem;">
+            <img class="card-img-top" src="static/img/flight.jpg" alt="Card image cap">
+            <div class="card-body">
+              <h5 class="card-title">${data[i]['from_']} → ${data[i]['to_']}</h5>
+              <h6 class="card-subtitle mb-2 text-muted">Rs.${data[i].price}</h6>
+              <p class="card-text">Flight Name : ${data[i]['name']}</p>
+            </div>
+            <ul class="list-group list-group-flush">
+              <li class="list-group-item">Date : ${date[0]}</li>
+              <li class="list-group-item">Time : ${data[i]['time']}</li>
+              <li class="list-group-item">Tickets : ${data[i]['tickets']}</li>
+            </ul>
+            
+            <div class="card-body">
+                <button class="btn btn-dark" onclick="remove_flight(${data[i]['id']})">Remove</button>
+            </div>
+          </div>`
+        }
+        else if(op == "view")
+        {
+            headDIV = document.getElementById("detailheader");
+            headDIV.style.display = "none";
+            const date = data[i]['date'].split(/(\s+)/);
+            var template = `<div class="card" style="width: 18rem;">
+            <img class="card-img-top" src="static/img/flight.jpg" alt="Card image cap">
+            <div class="card-body">
+              <h5 class="card-title">${data[i]['from_']} → ${data[i]['to_']}</h5>
+              <h6 class="card-subtitle mb-2 text-muted">Rs.${data[i].price}</h6>
+              <p class="card-text">Flight Name : ${data[i]['name']}</p>
+            </div>
+            <ul class="list-group list-group-flush">
+              <li class="list-group-item">Date : ${date[0]}</li>
+              <li class="list-group-item">Time : ${data[i]['time']}</li>
+              <li class="list-group-item">Tickets : ${data[i]['tickets']}</li>
+            </ul>
+            
+            <div class="card-body">
+                <button class="btn btn-dark" onclick="view(${data[i]['id']})">View</button>
+            </div>
+          </div>`
+        }
+
+      div.innerHTML += template;
+    }
+}
+
+async function search(op)
+{
+    var from = document.getElementById("from").value;
+    var to = document.getElementById("to").value;
+    var date = document.getElementById("date").value;
+    
+
+    var url = `http://localhost:5000/search?from=${from || " "}&to=${to || " "}&date=${date || " "}`;
+    
+    var body = 
+    {
+        from: from || " ",
+        to: to || " ",
+        date: date || " "
+    }
+    console.log(body);
+    try{
+        var response = await fetch(url, {
+            method: 'GET',
+            headers: {
+                'Content-Type': 'application/json'
+            }
+        });
+
+        if (response) {
+            var data = await response.json();
+            console.log(data);
+            if(data)
+            {
+                create_divs(data, op);
+            }
+
+            
+        }
+    
+    }
+    catch(error) {
+        console.log(error);
+    }
+}
+
+
+async function create_flight()
+{
+    var name = document.getElementById("name").value;
+    var from = document.getElementById("from").value;
+    var to = document.getElementById("to").value;
+    var date = document.getElementById("date").value;
+    var time = document.getElementById("time").value;
+    var price = document.getElementById("price").value;
+
+    var url = `http://localhost:5000/create_flight?name=${name}&from=${from}&to=${to}&date=${date}&time=${time}&price=${price}`;
+    var flight = {
+        name: name,
+        from: from,
+        to: to,
+        date: date,
+        time: time,
+        price: price
+    };
+
+    try{
+        var response = await fetch(url, {
+            method: 'PUT',
+            headers: {
+                'Content-Type': 'application/json'
+            },
+            body: JSON.stringify(flight)
+        });
+
+        if (response) {
+            var data = await response.json();
+            console.log(data);
+            alert("Flight created successfully");
+        }
+        else {
+            alert("Error creating flight");
+        }
+    }
+    catch(error) {
+        console.log(error);
+    }
+
+    // create api call
+    
+}
